52476a51501db24b8a849ddafdafc00b48fbeea4
Miyajima0828 <myjm1766@gmail.com>
2023-10-06 16:30:25 +0900

    migrate

---
+++ database/migrations/2023_09_19_210050_create_texts_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('texts', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('users_id');
+            $table->integer('main_id');
+            $table->integer('sub_id');
+            $table->string('text',10000)->nullable();
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('texts');
+    }
+};
