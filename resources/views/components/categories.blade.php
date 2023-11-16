efb790d368c44b69c98a96705dc60397a214b1c3
haru546 <harukahisakuro@gmail.com>
2023-11-16 10:11:17 +0900

    Committing local changes before pull

--- resources/views/components/categories.blade.php
+++ resources/views/components/categories.blade.php
@@ -1,5 +1,6 @@
-{{ $userMainCount = count($userMain) }}
-
+@php
+    $userMainCount = count($userMain)
+@endphp
 
 @foreach($userMain as $main)
     <p>{{$main}}</p>
@@ -9,23 +10,31 @@
 <!-- もしユーザーがDBにmain_idをcount=5持っていたら -->
 @if ($userMainCount == 3)
     @foreach ($mainIdArray as $mainId)
-        {{ $userSubCount = count($userSub[$mainId]) }}
-        @foreach ($userSub as $sub)
+        @php
+            $userSubCount = count($userSub[$mainId])
+        @endphp
 <!-- 各mainカテゴリーに該当するsubカテゴリーが5個未満なら、 -->
-            @if ($userSubCount < 5)
+        @if ($userSubCount < 5)
 <!-- 各mainカテゴリーの右端に＋マーク表示し、
 mainカテゴリーに該当するsubカテゴリーをmainカテゴリのpの下にp,smallで表示 -->
-                <h2>{{ $userMain[0] }}<span>＋</span></h2>
-                {{ $subitem = $sub['0']['sub'] }}
-                <p><small>{{ $subitem }}</small></p>
+            <h2>{{ $userMain[0] }}<span>＋</span></h2>
+            @foreach ($userSub as $sub)
+                @php
+                    $subitem = $sub[0]['sub'];
+                    echo '<p><small>'.$subitem.'</small></p>';
+                @endphp
+            @endforeach
 <!-- 各mainカテゴリーに該当するsubカテゴリーが5個なら、 -->
-            @else
+        @else
 <!-- 各mainカテゴリーを表示し、
 mainカテゴリーに該当するsubカテゴリーをmainカテゴリのpの下にp,smallで表示 -->
                 <h2>{{ $userMain[0] }}</h2>
-                {{ $subitem = $sub['0']['sub'] }}
+                @foreach ($userSub as $sub)
+                @php
+                    $subitem = $sub['0']['sub']
+                @endphp
                 <p><small>{{ $subitem }}</small></p>
-            @endif
-        @endforeach
+                @endforeach
+        @endif
     @endforeach
 @endif
